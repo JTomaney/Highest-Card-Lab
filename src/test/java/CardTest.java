0e23242309b2d7c2dc4677b765cae8855e576416
Jonathan Tomaney <jon.tomaney@gmail.com>
2019-06-13 13:03:54 +0100

    Card has suit and rank

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,2 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CardTest {
+
+    Card card;
+
+    @Before
+    public void before(){
+        card = new Card(SuitType.HEARTS, RankType.JACK);
+    }
+
+    @Test
+    public void canGetSuit(){
+        assertEquals(SuitType.HEARTS, card.getsuit());
+    }
+
+    @Test
+    public void canGetRank(){
+        assertEquals(RankType.JACK, card.getRank());
+    }
+
+    @Test
+    public void canGetRankValue(){
+        assertEquals();
+    }
+
 }
